40e6ee110b3e798a9a56670a87f53cb9b12b8c6c
HighOrder-Thinking <divya.sree.kodidela@gmail.com>
2026-02-17 15:04:17 +0530

    UC1: Created OOPSBannerApp to print OOPS to console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,20 @@
+/**
+ * OOPS Banner App - Use Case 1: Print OOPS to Console
+ * This class serves as the container for our application logic.
+ *
+ * @author [Enter Your Name Here]
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * The main method is the application's entry point.
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // Using System.out.println() to print the String Literal
+        System.out.println("OOPS");
+        
+    }
+}
