40c8add867e8347a5b9446dfa88527c66e614e2a
HighOrder-Thinking <divya.sree.kodidela@gmail.com>
2026-03-28 19:30:23 +0530

    UC2: Personalized greeting with command-line arguments

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,1 +1,11 @@
-public class HelloApp { public static void main(String[] args) { System.out.println("Hello World"); } }
+public class HelloApp {
+    public static void main(String[] args) {
+        // UC2: Check if a name was provided as a command-line argument
+        if (args.length > 0) {
+            System.out.println("Hello " + args[0]);
+        } else {
+            // Default behavior if no argument is given
+            System.out.println("Hello World");
+        }
+    }
+}
